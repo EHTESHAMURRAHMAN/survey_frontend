9d4fa996970ccd9845149fbc05869c03c351ad01
Ehtesham <ehteshamkhan2002@gmail.com>
2026-04-24 10:14:47 +0530

    ready

--- src/types/uiConfig.ts
+++ src/types/uiConfig.ts
@@ -2,7 +2,7 @@ export interface SurveyUIConfig {
   config: {
     backgroundImage: string;
     poweredBy: { logo: string };
-    disclaimer: { text: string };
+    disclaimer: { text: string, emailFieldName?: string; };
     checkbox: { text: string };
   }
 }
